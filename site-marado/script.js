984d43cbd9fb7d9865ac209fffacfc310e9749d4
n1ckyz1n <mariana167gaia@gmail.com>
2025-10-02 22:58:46 +0100

    Add files via upload

---
+++ site-marado/script.js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+            const mensagem = document.getElementById('butãoSuperProibido');
+            mensagem.textContent = 'tu clicaste em mim... tu não deverias ter clicado... agora,eu adicionei um "virus" que é executado quando uma ação expecifica é feita, como por exemplo, se abrires os ficheiros, mas não te preocupes, isto tem salvação, é só carregares no botão direito do mouse e clicares em "inspecionar" e encontrar o link escondido nesse monte de codigo, tens 10 minutos, senão, todos os teus dados serão roubados, mua hahahahhahahaha'
+}
